355c71067b3d43fa7f1576507bf569ac406f2dbb
unknown <you@example.com>
2025-07-12 23:56:45 +0600

    team single test

--- pamar-html/build/inc/sections/team-details-v1.php
+++ pamar-html/build/inc/sections/team-details-v1.php
@@ -3,7 +3,8 @@ Team Area
 ==============================-->
 <section class="<?php echo $klass; ?>">
     <div class="container">
-        <div class="row gx-40 gy-40">
+        <div class="teamDetails">
+            <div class="row gx-40 gy-40">
             <div class="col-xxl-4 col-xl-4 col-xl-6">
                 <div class="th-team team-card style2 style4">
                     <div class="team-img">
@@ -28,14 +29,14 @@ Team Area
                     </div>
                 </div>
             </div>
-            <div class="col-xxl-8 col-xl-8 ">
+            <div class="col-xxl-8 col-xl-8">
                 <div class="page-single">
                     <h2 class="page-title mt-n2 mb-20">Personal Information:</h2>
                     <p class="mb-0">Hello, This Jordan Smith , Professional about variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. 
                     </p>
                     <p class="mb-20">in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                     <p>“Appreciated the courtesy & promptness to addressing my issues. Worked with a sense of urgency for a resolution in my unplanned plumbing emergency. Repairs were done in a timely, professional manner and with thoroughness & care by the experienced technician.”</p>
-                   <div class="checklist list-three-column style5 mb-50 mt-40">
+                   <div class="checklist list-three-column style5 mb-50 mt-35">
                         <ul>
                             <li>Best Quality services Provided</li>
                             <li>24/7 Support and Assistance</li>
@@ -81,16 +82,67 @@ Team Area
                         </div>
                     </div>
                 </div>
-
             </div>            
         </div>
-        <div class="row gx-40 gy-40">
-            <div class="col-xxl-8 col-xl-8 ">
-                
+        <div class="row gx-40 gy-70">
+            <div class="col-xxl-8 col-xl-8">
+                <div class="box-content">
+                    <h2 class="box-title">Professional Skills</h2>
+                    <p class="mb-20">The culmination of this transformative endeavor marked the dawn of a new era for the office space, where meticulous attention to detail and an unwavering commitment to excellence converged to create an environment that transcended mere functionality.</p>
+                    <p class="mb-50">Through an intricate and strategic overhaul of the office's electrical infrastructure, our dedicated team orchestrated a symphony of technological innovation and precision engineering,</p>
+                </div>
+                <div class="skill-wrap">
+                    <div class="skill-feature style3">
+                        <div class="skill-feature">
+                            <h3 class="skill-feature_title">High Engagement <span>90%</span></h3>
+                            <div class="progress">
+                                <div class="progress-bar bg-theme" style="width: 90%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill-feature style3">
+                        <div class="skill-feature">
+                            <h3 class="skill-feature_title">Effective Project Manage <span>80%</span></h3>
+                            <div class="progress">
+                                <div class="progress-bar bg-theme" style="width: 80%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill-feature style3">
+                        <div class="skill-feature">
+                            <h3 class="skill-feature_title">Consulting & Planning <span>85%</span></h3>
+                            <div class="progress">
+                                <div class="progress-bar bg-theme" style="width: 85%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill-feature style3">
+                        <div class="skill-feature">
+                            <h3 class="skill-feature_title">High Communication Skilled <span>95%</span></h3>
+                            <div class="progress">
+                                <div class="progress-bar bg-theme" style="width: 95%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>   
             <div class="col-xxl-4 col-xl-4 col-xl-6">
-                
+                <div class="testi-card">
+                    <div class="d-flex justify-content-between align-items-center mb-24">
+                        <div class="box-icon">
+                            <img src="assets/img/icon/quote-icon5.svg" alt="">
+                        </div>
+                        <div class="testi-card_review">
+                            <i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>
+                        </div>
+                    </div>
+                    <p class="box-text">Communication before and during the repair was helpful,
+                         since we were unsure how extensive it might be once the area was dug out. 
+                        We would highly recommend both Mike and Chris. Repairs were done in a timely,</p>
+                    
+                </div>
             </div>     
+        </div>
         </div>
     </div>
 </section>
